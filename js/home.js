b2206cdc055a1a90899ba6aa869b062f5651c857
Mohammad Omar Shihab <lonak.secret@gmail.com>
2021-03-01 15:01:22 +0200

    Latest Changes

--- js/home.js
+++ js/home.js
@@ -60,3 +60,21 @@ fadeInElement(headerTitle,4)
 slideUpFading(headerTitle_p,1)
 slideUpFading(scrollDownBtn,2)
 
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
