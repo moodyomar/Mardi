98d4b38b6973cb48c3808a128c5a795273db8a90
Moody Omar <lonak.secret@gmail.com>
2021-03-10 15:42:08 +0200

    mainly form validations

---
+++ js/contact.js
@@ -0,0 +1,72 @@
+const phoneInput = document.querySelector("input[type='tel']");
+const mailInput = document.querySelector("input[type='email']");
+const nameInput = document.querySelector("#name");
+const textarea = document.querySelector("textarea");
+const submit = document.querySelector("#submit");
+const validPhone = /^[0-9]+$/;
+const validMail = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+const validName = /^[a-zA-Z]{2,}(?: [a-zA-Z]+){0,2}$/;
+const $popupOverlay = $('.popupOverlay')
+const $popup = $('.popup')
+
+
+submit.addEventListener("click", () => {
+
+inputChecker()
+
+})
+
+const inputChecker = function () {
+    let allGood = 0;
+    if (phoneInput.value.length == 10 && phoneInput.value.match(validPhone)) {
+        valid(phoneInput)
+        allGood++;
+    } else {
+        notValid(phoneInput)
+    }
+    if (mailInput.value.match(validMail)) {
+        valid(mailInput)
+        allGood++;
+    } else {
+        notValid(mailInput)
+    }
+    if(nameInput.value.match(validName)) {
+        valid(nameInput)
+        allGood++;
+    }else {
+        notValid(nameInput)
+    }
+    if(textarea.value != '') {
+        valid(textarea)
+        allGood++;
+    }else {
+        notValid(textarea)
+    }
+    console.log(allGood)
+    if(allGood == 4){
+        msg = `<h2>Dear ${nameInput.value},</h2> <br> <p> Thank you for choosing to contact us.<br> We have got your message and will get back to you ASAP. <br><br>
+        Mardi Gras Team.</p>
+        <br><img src="images/site-images/logo-small.png" style="height: 100px; width:150px; margin-top: 80px;">`
+        showMsg(msg);
+    }
+}
+
+const notValid = function(input) {
+input.style.border = "1px solid red"
+}
+const valid = function(input) {
+    input.style.border = "1px solid green"
+    }
+
+const showMsg = function(msg){
+    $popupOverlay.fadeIn(500)
+    $popup.html(msg)
+    setTimeout(() => {
+        $popupOverlay.fadeOut(400)
+    },3500)
+}
+
+
+$popupOverlay.on('click', () => {
+    $popupOverlay.fadeOut(200)
+})
---
+++ js/trivia.js
@@ -0,0 +1,42 @@
+const trivia = [{
+    question: "What is Mardi Gras ?",
+    answers: ["answer one","Mardi Gras","answer three","answer four"],
+    correctAnswer: "Mardi Gras"
+},{
+    question: "How much does it cost to enter ?",
+    answers: ["answer one","Gras Mardi","answer three","answer four"],
+    correctAnswer: "Gras Mardi"
+},{
+    question: "Where is Mardi Gras ?",
+    answers: ["answer one","Holy Mary","answer three","answer four"],
+    correctAnswer: "Holy Mary"
+},{
+    question: "when is Mardi Gras ?",
+    answers: ["answer one","Magic","answer three","answer four"],
+    correctAnswer: "Magic"
+},{
+    question: "Who started Mardi Gras ? ?",
+    answers: ["answer one","Spell","answer three","answer four"],
+    correctAnswer: "Spell"
+},{
+    question: "Who is the mayor of New Orleans Now? ?",
+    answers: ["answer one","Snake","answer three","answer four"],
+    correctAnswer: "Snake"
+},{
+    question: "What is the most common colors in Mardi Gras ?",
+    answers: ["answer one","Binky","answer three","answer four"],
+    correctAnswer: "Binky"
+},{
+    question: "How long does Mardi Gras last ?",
+    answers: ["answer one","Moody","answer three","answer four"],
+    correctAnswer: "Moody"
+},{
+    question: "What is Mardi Gras 9 ?",
+    answers: ["answer one","JavaScript","answer three","answer four"],
+    correctAnswer: "JavaScript"
+},{
+    question: "What is Mardi Gras 10 ?",
+    answers: ["answer one","Objects","answer three","answer four"],
+    correctAnswer: "Objects"
+}]
+
